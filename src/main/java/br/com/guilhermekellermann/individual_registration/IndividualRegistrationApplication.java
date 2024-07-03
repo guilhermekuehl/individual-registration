0335d924537b14927b889e7c01dd75015e847679
guilhermekuehl <guikuehlkeller120@gmail.com>
2024-07-03 14:49:51 -0300

    first commit

---
+++ src/main/java/br/com/guilhermekellermann/individual_registration/IndividualRegistrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.guilhermekellermann.individual_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividualRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guilhermekellermann/individual_registration/IndividualRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilhermekellermann.individual_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
